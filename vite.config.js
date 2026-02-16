ea82bebaded85ee96f69b6b751ad3bc959b31c94
Sujanraja <rsuji521@gmail.com>
2026-02-16 23:56:18 +0530

    sujan

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sujandailyplannerfinal/',   // 👈 must match GitHub repo name
+  base: '/sujandailyplannerfinalweb/',   // 👈 must match GitHub repo name
 });
